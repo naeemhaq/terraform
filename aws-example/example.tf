5c9da98e17b4b047ca51dccae17c982fea29017e
naeemu <naeem_ulhaq@trendmicro.com>
2019-09-19 11:36:48 -0400

    terraform create vm template

---
+++ aws-example/example.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    profile = "naeem"
+    region = var.region
+}
+
+resource "aws_instance" "example" {
+    ami = var.amis[var.region]
+    instance_type = "t2.micro"
+
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+    }
+}
+
+resource "aws_eip" "eip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+output "ip" {
+  value = aws_instance.example.public_ip
+}
